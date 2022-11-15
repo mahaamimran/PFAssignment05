98891ba62db64d108d7f79bce42925a13182ec71
Maham Imran <mahaamimran@gmail.com>
2022-11-15 19:59:50 +0500

    Initial Commit

---
+++ F_22i2733_05/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  F_22i2733_05
+//
+//  Created by Maham Imran on 15/11/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
